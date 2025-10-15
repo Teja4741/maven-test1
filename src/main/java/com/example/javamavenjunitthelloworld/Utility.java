60476d370e1a538253f95352fd475996ecb5af1b
Saiteja4741 <mrteja2809@gmail.com>
2025-10-16 00:50:00 +0530

    TEJAV0001 - Wrote the code for odd/even check in Utility.java

---
+++ src/main/java/com/example/javamavenjunitthelloworld/Utility.java
@@ -0,0 +1,14 @@
+package com.example.javamavenjunitthelloworld;
+
+public class Utility {
+
+    // Example function for assignment: Check if the given number is odd or even
+    public void tejaOddEven(int number) {
+        System.out.println("I am Teja");
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+}
